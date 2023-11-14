ea60056a10d83de58e7075e3a8a70421774391d6
Le Quoc Tuan <lequoctuan.qt@gmail.com>
2023-11-14 13:47:08 +0700

    Finished 3.2

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="style/login.css">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="style/style.css">
         <script src="./script.js"></script>
     </head>
     
