0f73df2107a559d73cf5a316dd005870222d4643
Le Quoc Tuan <lequoctuan.qt@gmail.com>
2023-11-14 08:31:10 +0700

    Finished login validation client and server side

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -9,37 +9,46 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="image/x-icon" href="logo_1.jpg">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link href="./style.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style/login.css">
+        <link rel="stylesheet" href="style.css">
         <script src="./script.js"></script>
     </head>
-
+    
     <body>
-        <?php include './nav_bar/nav.php' ?>
-
-        <div class="container w-25 mt-5">
-            <form name="loginForm" action="./login_processing.php" onsubmit="return validateLoginForm()" method="post">
-                <div class="mb-3 mt-3">
-                    <label for="username" class="form-label" style="font-weight:700">Tên đăng nhập:</label>
-                    <input type="text" class="form-control" id="username" placeholder="Nhập vào tên đăng nhập" name="username" value="<?php echo isset($_SESSION["username"]) ? $_SESSION["username"] : "" ?>">
-                </div>
-                <div class="mb-3 mt-3">
-                    <label for="pwd" class="form-label" style="font-weight:700">Mật khẩu:</label>
-                    <input type="password" class="form-control" id="pwd" placeholder="Nhập vào mật khẩu" name="pswd">
-                </div>
-                <button type="submit" class="btn btn-warning">Đăng nhập</button>
-            </form>
-
-            <div class="container-fluid mt-2">
-                <?php
-                    if (isset($_GET['wrong_pwd'])) {
-                        echo "Sai mật khẩu, vui lòng thử lại.";
-                    }
-                    if (isset($_GET['username_not_found'])) {
-                        echo "Không tìm thấy tên đăng nhập";
-                    }
-                ?>
+        <div class="container-fluid" id="body_container">
+            <div class="container" id="form_container">
+                <form name="loginForm" action="./login_processing.php" onsubmit="return validateLoginForm()" method="post">
+                    <div class="mb-3 mt-3">
+                        <label for="username" class="form-label" style="font-weight:700">Tên đăng nhập:</label>
+                        <input type="text" 
+                            class="form-control" 
+                            id="username" 
+                            placeholder="Nhập vào tên đăng nhập" 
+                            name="username" 
+                            value="<?php echo isset($_SESSION["username"]) ? $_SESSION["username"] : "" ?>">
+                    </div>
+                    <div class="mb-3 mt-3">
+                        <label for="pwd" class="form-label" style="font-weight:700">Mật khẩu:</label>
+                        <input type="password" 
+                            class="form-control" 
+                            id="pwd" 
+                            placeholder="Nhập vào mật khẩu" 
+                            name="pswd">
+                    </div>
+                    <button type="submit" class="btn btn-warning">Đăng nhập</button>
+                    <div id="login_failed">
+                        <?php
+                        if (isset($_GET['wrong_pwd'])) {
+                            echo "Sai mật khẩu, vui lòng thử lại.";
+                        }
+                        if (isset($_GET['username_not_found'])) {
+                            echo "Không tìm thấy tên đăng nhập";
+                        }
+                        ?>
+                    </div>
+                </form>
             </div>
         </div>
     </body>
